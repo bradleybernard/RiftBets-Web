32a241741a10b6383d7d68e28d8daabab2729dd0
Christopher Huynh <chphuynh@ucsc.edu>
2016-10-12 21:39:08 -0700

    added queries folder

---
+++ app/Http/Controllers/Queries/MatchController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Queries;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use DB;
+
+class MatchController extends Controller
+{
+    //
+}
