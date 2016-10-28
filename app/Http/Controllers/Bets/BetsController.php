cb51618757858f6c73d9ab4d252a78fd9a77cfa3
Brad Bernard <bradbernard@me.com>
2016-10-27 20:41:52 -0700

    Testing bet place

--- app/Http/Controllers/Bets/BetsController.php
+++ app/Http/Controllers/Bets/BetsController.php
@@ -6,10 +6,18 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Validator;
 use DB;
 
 class BetsController extends Controller
 {
+	protected $games;
+
+	private function isBeforeGameTime($games, $match)
+	{
+
+	}
+
 	public function bet(Request $request)
 	{
 		/*
@@ -18,18 +26,52 @@ class BetsController extends Controller
 		bets[0][user_answer]:2222
 		bets[0][credits_placed]:500
 		*/
-
-		$user = $this->auth->user();
-
-		$games = DB::table('games')->whereIn('api_id_long', $request->input('bets.*.api_game_id'))->get();
-		dd($games);
+		$request->merge(['user_credits' => $this->auth->user()->credits]);
 
 		$validator = Validator::make($request->all(), [
-		    'bets.*.api_game_id' 		=> 'required|exists:games,api_id_long',
-		    'bets.*.question_slug' 		=> 'required|exists:questions,slug',
+			'bets.*'					=> 'required',
+		    'bets.*.api_game_id' 		=> 'required|same:bets.*.api_game_id',
+		    'bets.0.api_game_id'		=> 'exists:games,api_id_long',
+		    'bets.*.question_slug' 		=> 'required|distinct',
 		    'bets.*.user_answer' 		=> 'required',
-		    'bets.*.credits_placed' 	=> 'required|integer|min:1|max:' . $user->credits,
+		    'bets.*.credits_placed' 	=> 'required|integer|min:1',
+		    'user_credits'				=> 'integer|min:' . count($request->input('bets.*')),
 		]);
+
+		if ($validator->fails()) {
+            throw new \Dingo\Api\Exception\ResourceException('Invalid request sent.', $validator->errors());
+        }
+
+		$sum = collect($request->input('bets.*.credits_placed'))->sum();
+		$request->merge(['credits_placed' => $sum]);
+
+		$perBetMaximum = ($this->auth->user()->credits - count($request->input('bets.*'))) + 1;
+
+		$validator = Validator::make($request->all(), [
+		    'bets.*.question_slug' 		=> 'required|exists:questions,slug',
+		    'bets.*.credits_placed' 	=> 'required|integer|min:1|max:' . $perBetMaximum,
+		    'credits_placed'			=> 'required|integer|max:' . $this->auth->user()->credits,
+		]);
+
+		if ($validator->fails()) {
+            throw new \Dingo\Api\Exception\ResourceException('Invalid request sent.', $validator->errors());
+        }
+
+        $match = DB::table('games')->select('api_match_id')
+        			->where('api_id_long', $request->input('bets.0.api_game_id'))
+        			->first();
+
+        $games = DB::table('schedule')
+        			->select('games.*')
+					->join('matches', 'matches.api_id_long', '=', 'schedule.api_match_id')
+					->join('games', 'games.api_match_id', '=', 'matches.api_id_long')
+					->where('matches.api_id_long', $match->api_match_id)
+					->get()
+					->keyBy('api_id_long');
+
+		if(!$games) {
+			throw new \Dingo\Api\Exception\ResourceException('Invalid match ID.', $validator->errors());
+		}
 
 		// $betId = DB::table('bets')->insertGetId([
 		// 	'user_id'			=> $request['user_id'],
