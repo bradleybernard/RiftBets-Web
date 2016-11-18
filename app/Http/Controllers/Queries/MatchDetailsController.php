8381505c8e7b04a09ed8f33c237f65017b9f75f7
Christopher Huynh <chphuynh@ucsc.edu>
2016-11-17 20:48:07 -0800

    MatchDetails returns next bettable game

--- app/Http/Controllers/Queries/MatchDetailsController.php
+++ app/Http/Controllers/Queries/MatchDetailsController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 
 use DB;
 use Validator;
+use \Carbon\Carbon;
 
 class MatchDetailsController extends Controller
 {
@@ -23,12 +24,19 @@ class MatchDetailsController extends Controller
             throw new \Dingo\Api\Exception\ResourceException('Invalid match id.', $validator->errors());
         }
 
-    	$columns = ['matches.api_id_long', 'matches.name', 'resource_type', 'matches.api_resource_id_one', 'matches.api_resource_id_two',
+    	$columns = ['matches.api_id_long', 'matches.name', 'resource_type', 'matches.state','matches.api_resource_id_one', 'matches.api_resource_id_two',
     			 'matches.score_one', 'matches.score_two'];
 
     	$rows = DB::table('matches')->select($columns)
     			->where('matches.api_id_long', $matchId)
     			->get();
+
+        $bettable = $this->isMatchBettable($request, false);
+
+        $rows->transform(function ($item, $key) use ($bettable) {
+            $item->bettable_game = $bettable;
+            return $item;
+        });
 
     	$filtered = $rows->filter(function ($value, $key) {
             return $value->resource_type == 'roster';
@@ -299,8 +307,103 @@ class MatchDetailsController extends Controller
             });
         }
 
-        // dd($rows->toArray());
-
         return $this->response->array((array)$rows->first());
+    }
+
+    public function isMatchBettable(Request $request, $isRoute = true)
+    {
+        $matchId = $request['match_id'];
+
+        $validator = Validator::make($request->all(), [
+            'match_id' => 'exists:matches,api_id_long'
+        ]);
+
+        if ($validator->fails()) {
+            throw new \Dingo\Api\Exception\ResourceException('Invalid match id.', $validator->errors());
+        }
+
+        $match = DB::table('matches')->select('state')
+                    ->where('api_id_long', $matchId)
+                    ->get()
+                    ->first();
+
+        $nextGame = collect([]);
+
+        $currentTime = Carbon::now();
+
+        if($match->state == 'resolved')
+        {
+            $nextGame = null;
+        } 
+        else
+        {
+            $games = DB::table('games')->select(['name as game_name', 'game_id'])
+                        ->where('api_match_id', $matchId)
+                        ->get()
+                        ->keyBy('game_name');
+
+            $games = $games->filter(function ($value, $key) {
+                return $value->game_id !== null;
+            });
+
+            if(!$games->first())
+            {
+                $gameStart = DB::table('schedule')->select('scheduled_time')
+                                ->where('api_match_id', $matchId)
+                                ->get()
+                                ->first();
+
+                $gameStart = Carbon::parse($gameStart->scheduled_time);
+
+                $gameStart->addMinutes(5);
+
+                if($currentTime->diffInMinutes($gameStart, false) < 0)
+                {
+                    $nextGame = null;
+                }
+                else
+                {
+                    $nextGame = collect([
+                        'game_name'         => 'G1',
+                        'bettable_until'    => $gameStart->toDateTimeString(),
+                    ]);
+                }
+            }
+            else
+            {
+                $lastGame = max($games->keys()->toArray());
+
+                $gameStart = DB::table('game_mappings')->select('created_at')
+                                ->where('game_id', $games[$lastGame]->game_id)
+                                ->get()
+                                ->first();
+
+                $gameStart = Carbon::parse($gameStart->created_at);
+
+                $gameStart->addMinutes(15);
+
+                if($currentTime->diffInMinutes($gameStart, false) < 0)
+                {
+                    $nextGame = null;
+                }
+                else
+                {
+                    $nextGame = collect([
+                        'game_name'         => $lastGame,
+                        'bettable_until'    => $gameStart->toDateTimeString(),
+                    ]);
+                }
+            }
+            
+        }
+
+        if($isRoute)
+        {
+            return $this->response->array($nextGame->first());
+        }
+        else
+        {
+            return $nextGame;
+        }
     }
 }
